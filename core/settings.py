fc61b09260461dddae1679ba51996a279714eace
jaseer j <jassjaseerdazzils2000@gmail.com>
2023-07-04 00:53:21 +0530

    commit

--- core/settings.py
+++ core/settings.py
@@ -156,14 +156,7 @@ SIMPLE_JWT = {
     'TOKEN_TYPE_CLAIM': 'token_type',
 }
 
-CORS_ORIGIN_ALLOW_ALL = True
-CORS_ALLOW_CREDENTIALS = True
 
-CORS_ORIGIN_ALLOW_ALL = False
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:5173',  # Your React app's development server
-    'http://127.0.0.1:8000',  # Add your Django local server
-]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
